52b21def725664d8e468131d8e1d027681e6f850
DarshilDhandhiya <darshil.dhandhiya03@gmail.com>
2023-08-31 16:44:59 +0530

    commit on 2023-08-31 16:44:59

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import os
+from datetime import datetime, timedelta
 
-target_dates = ["2023-07-30", "2023-07-31", "2023-07-23", "2023-07-07"]
+total_commits_to_add = 11
 
-for target_date in target_dates:
-    # Create a commit for the target date
-    d = target_date + " 00:00:00"
+# Get the current date
+current_date = datetime.now()
+
+for i in range(total_commits_to_add):
+    # Create a commit date
+    commit_date = current_date - timedelta(days=i)
+    d = commit_date.strftime("%Y-%m-%d %H:%M:%S")
+    
+    # Write commit date to file and make a commit
     with open('file.txt', 'a') as file:
         file.write(d + '\n')
     os.system('git add .')
-    os.system(f'git commit --date="{d}" -m "commit on {target_date}"')
+    os.system(f'git commit --date="{d}" -m "commit on {d}"')
 
 # Push the commits to the repository
 os.system('git push -u origin main')
